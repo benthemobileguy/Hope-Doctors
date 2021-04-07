3ae0618051d4170cb94e7f60c6bd3709a59e5daf
Ben <benbright065@gmail.com>
2021-04-07 02:31:05 +0100

    screens

--- lib/screens/health-tips/index.dart
+++ lib/screens/health-tips/index.dart
@@ -110,43 +110,246 @@ class _HealthTipsScreenState extends State<HealthTipsScreen> {
             SizedBox(
               height: 30,
             ),
-            Container(
-              width: 240,
-              padding: EdgeInsets.symmetric(horizontal: 12, vertical:12 ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all
-                    (Radius.circular(8)),
-                  border: Border.all(color: Color.fromRGBO
-                    (7, 121, 101, 0.8), width: 1)
-              ),
-              child:  Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "TITLE",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontFamily: 'Lato',
-                      color: darkerText,
-                      fontWeight: FontWeight.w700,
-                    ),
+            Row(
+              children: [
+                Container(
+                  width: 160,
+                  height: 130,
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical:12 ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all
+                        (Radius.circular(8)),
+                      border: Border.all(color: Color.fromRGBO
+                        (7, 121, 101, 0.8), width: 1)
                   ),
-                  SizedBox(
-                    height: 10,
+                  child:  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "TITLE",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontFamily: 'Lato',
+                          color: darkerText,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Back Pain",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: 'Lato',
+                          color: normalTextBold,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "VIEWS",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Lato',
+                              color: darkerText,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            "MEDIA",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Lato',
+                              color: darkerText,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "32",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 14.5,
+                              fontFamily: 'Lato',
+                              color: greyColor2,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 44,
+                          ),
+                          Text(
+                            "6 FILES",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 14.5,
+                              fontFamily: 'Lato',
+                              color: greyColor2,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  Text(
-                    "Back Pain",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      fontSize: 23,
-                      fontFamily: 'Lato',
-                      color: normalTextBold,
-                      fontWeight: FontWeight.w400,
-                    ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  width: 160,
+                  height: 130,
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical:12 ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all
+                        (Radius.circular(8)),
+                      border: Border.all(color: Color.fromRGBO
+                        (7, 121, 101, 0.8), width: 1)
                   ),
-                ],
-              ),
+                  child:  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "TITLE",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontFamily: 'Lato',
+                          color: darkerText,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Back Pain",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: 'Lato',
+                          color: normalTextBold,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "VIEWS",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Lato',
+                              color: darkerText,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            "MEDIA",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Lato',
+                              color: darkerText,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "32",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 14.5,
+                              fontFamily: 'Lato',
+                              color: greyColor2,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 44,
+                          ),
+                          Text(
+                            "6 FILES",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 14.5,
+                              fontFamily: 'Lato',
+                              color: greyColor2,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  width: 160,
+                  height: 130,
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical:12 ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all
+                        (Radius.circular(8)),
+                      border: Border.all(color: Color.fromRGBO
+                        (7, 121, 101, 0.8), width: 1)
+                  ),
+                  child:  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      new Image.asset("images/add_circle.png",
+                        width: 64,
+                        height: 64,),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "ADD NEW",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontFamily: 'Lato',
+                          color: darkerText,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
 
           ],
