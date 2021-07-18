d61785a9e32f63f061d9c97fb60fc4e9c7636a43
User <apple>
2021-07-18 18:10:50 +0100

    marked map

--- lib/screens/shop/components/shop-item.dart
+++ lib/screens/shop/components/shop-item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hope_doctor/bloc/default.dart';
+import 'package:hope_doctor/screens/shop/shop-details-screen.dart';
 import 'package:hope_doctor/theme/style.dart';
 import 'package:provider/provider.dart';
 class ShopItem extends StatelessWidget {
@@ -19,144 +20,151 @@ class ShopItem extends StatelessWidget {
         ),
         itemCount: mainBloc.marketShop.length + 1,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-            child:  index == mainBloc.marketShop.length?
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 6, vertical:6 ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all
-                    (Radius.circular(8)),
-                  border: Border.all(color: Color.fromRGBO
-                    (7, 121, 101, 0.8), width: 1)
-              ),
-              child:  Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+          return GestureDetector(
+            onTap: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context)
+                  => ShopDetailsScreen(index: index)));
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child:  index == mainBloc.marketShop.length?
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 6, vertical:6 ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all
+                      (Radius.circular(8)),
+                    border: Border.all(color: Color.fromRGBO
+                      (7, 121, 101, 0.8), width: 1)
+                ),
+                child:  Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    new Image.asset("images/add_circle.png",
+                      width: 64,
+                      height: 64,),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "ADD NEW",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontFamily: 'Lato',
+                        color: darkerText,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ):Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  new Image.asset("images/add_circle.png",
-                    width: 64,
-                    height: 64,),
+                  Container(
+                    height: 130,
+                    width: 240,
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        border: Border.all(color:
+                        primaryColor.withOpacity(0.3),
+                            width: 2),
+                        image: DecorationImage(
+                            fit: BoxFit.contain,
+                            image: NetworkImage(
+                              "${mainBloc.marketShop[index].files[0]}",
+                            ))),
+                  ),
                   SizedBox(
                     height: 5,
                   ),
-                  Text(
-                    "ADD NEW",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontFamily: 'Lato',
-                      color: darkerText,
-                      fontWeight: FontWeight.w700,
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      "${mainBloc.marketShop[index].title}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Lato',
+                        color: normalTextBold,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      children: [
+                        Text(
+                          "AVAILABLE IN STOCK",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontFamily: 'Lato',
+                            color: darkerText,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 35,
+                        ),
+                        Text(
+                          "RESERVED PRODUCTS",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontFamily: 'Lato',
+                            color: darkerText,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "${mainBloc.marketShop[index].quantity}",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'Lato',
+                            color: normalTextBold,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 107,
+                        ),
+                        Text(
+                          "0",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'Lato',
+                            color: normalTextBold,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ):Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 130,
-                  width: 240,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      border: Border.all(color:
-                      primaryColor.withOpacity(0.3),
-                          width: 2),
-                      image: DecorationImage(
-                          fit: BoxFit.contain,
-                          image: AssetImage(
-                            "images/img_1.png",
-                          ))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    "${mainBloc.marketShop[index].title}",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Lato',
-                      color: normalTextBold,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
-                    children: [
-                      Text(
-                        "AVAILABLE IN STOCK",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontFamily: 'Lato',
-                          color: darkerText,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 35,
-                      ),
-                      Text(
-                        "RESERVED PRODUCTS",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontFamily: 'Lato',
-                          color: darkerText,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 3,
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        "${mainBloc.marketShop[index].quantity}",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontFamily: 'Lato',
-                          color: normalTextBold,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 107,
-                      ),
-                      Text(
-                        "0",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontFamily: 'Lato',
-                          color: normalTextBold,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
             ),
           );
 
---
+++ lib/screens/shop/shop-details-screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class ShopDetailsScreen extends StatefulWidget {
+  final int index;
+  const ShopDetailsScreen({Key key, this.index}) : super(key: key);
+
+  @override
+  _ShopDetailsScreenState createState() => _ShopDetailsScreenState();
+}
+
+class _ShopDetailsScreenState extends State<ShopDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
