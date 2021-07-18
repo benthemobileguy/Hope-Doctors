778afa01e91ce0ef560aa0769d6d11ab55196214
User <apple>
2021-07-18 18:12:40 +0100

    marked map

--- lib/screens/shop/shop-details-screen.dart
+++ lib/screens/shop/shop-details-screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hope_doctor/bloc/default.dart';
+import 'package:hope_doctor/theme/style.dart';
+import 'package:hope_doctor/utils/color.dart';
+import 'package:provider/provider.dart';
 class ShopDetailsScreen extends StatefulWidget {
   final int index;
   const ShopDetailsScreen({Key key, this.index}) : super(key: key);
@@ -8,10 +12,28 @@ class ShopDetailsScreen extends StatefulWidget {
 }
 
 class _ShopDetailsScreenState extends State<ShopDetailsScreen> {
+  MainBloc mainBloc;
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Shop",
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: 'Lato',
+            color: normalTextBold,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
     );
   }
 }
